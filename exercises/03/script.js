2e4c0bf5d31eb0f47ed5777374988d75b9ff39dd
rsantervastorres <rsantervastorres@al.uloyola.es>
2025-10-17 11:01:53 +0200

    Update script.js

--- exercises/03/script.js
+++ exercises/03/script.js
@@ -20,7 +20,7 @@ addButton.addEventListener('click', () => {
 
     const listItem = document.createElement('li');
 
-    const textNode = document.createTextNode(`{name}€: $${amount.toFixed(2)}`);
+    const textNode = document.createTextNode(`${name}: ${amount.toFixed(2)}€`);
     listItem.appendChild(textNode);
     
     expenseList.appendChild(listItem);
@@ -28,4 +28,5 @@ addButton.addEventListener('click', () => {
     expenseNameInput.value = '';
     expenseAmountInput.value = '';
 });
+
 
