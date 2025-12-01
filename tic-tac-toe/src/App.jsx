68e85e42514aeef3bdcc4b758f7ef4c76f779872
marinagarciaguado <marinagarciaguado@gmail.com>
2025-12-01 13:49:44 +0100

    merge and create 05 exercise

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -5,3 +5,17 @@ console.log('Hello');
 return <h1>Tic Tac Toe</h1>;
 }
 export default App;
+
+import './App.css';
+function Square() {
+return <button className="square">X</button>;
+}
+function App() {
+return (
+<>
+<h1>Tic Tac Toe</h1>
+<Square />
+</>
+);
+}
+export default App;
